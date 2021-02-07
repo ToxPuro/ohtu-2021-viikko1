b5945b4ffb472d76f6b34148349182edc5bf275f
ToxPuro <puro.touko2@gmail.com>
2021-02-07 16:30:08 +0200

    Testit eivät toimi nyt

--- src/varasto.py
+++ src/varasto.py
@@ -10,6 +10,17 @@ class Varasto:
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
+        print("a")
+        print("a")
+        print("a")
+        print("a")
+        print("a")
+        print("a")
+        print("a")
+        print("a")
+        print("a")
+        print("a")
+        print("a")
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
